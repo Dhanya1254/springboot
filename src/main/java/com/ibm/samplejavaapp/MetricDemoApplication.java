aa617d8a435660a986879d48c628a65bf8fbb4df
Dhanya134 <116253472+Dhanya134@users.noreply.github.com>
2022-12-20 14:38:17 +0530

    Update MetricDemoApplication.java

--- src/main/java/com/ibm/samplejavaapp/MetricDemoApplication.java
+++ src/main/java/com/ibm/samplejavaapp/MetricDemoApplication.java
@@ -4,12 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestController
+
 public class MetricDemoApplication {
-	@GetMapping("/messages")
-	public String getMessage() {
-		return "Hello from Docker!";
-	}
+	
 
 
 	public static void main(String[] args) {
